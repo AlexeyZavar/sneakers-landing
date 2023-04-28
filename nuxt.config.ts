8047a4bad0192e23772360333204688a26102e88
ZavaruKitsu <alexeyzavar@gmail.com>
2023-04-29 00:05:26 +0300

    fix: set viewport & baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    // @ts-ignore
+    app: {
+        head: {
+            meta: [
+                {name: 'viewport', content: 'width=1920px'}
+            ]
+        },
+        baseURL: '/shared/fl_2904'
+    },
     modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-icon'],
     googleFonts: {
         families: {
